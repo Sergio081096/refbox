e9338f82603f7067f4503bddab04eb3dbed58096
sergio <sergio081096@gmail.com>
2025-06-20 06:18:54 -0600

    update

--- local_setup.sh
+++ local_setup.sh
@@ -1,2 +1,6 @@
 export REFBOX_CONFIG_CHALLENGE=./../config/refbox/game/rc2024_test.yaml
 
+export RC_AUTO_SETUP=true
+export RC_AUTO_START=true
+export RC_CYAN="TuEquipoCyan"
+export RC_MAGENTA="TuEquipoMagenta"
